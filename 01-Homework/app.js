4f5e8bf75aa9ed3b53f69d1fd4e893caf6208159
PavlinkaA <pavlinkaatanasova@yahoo.com>
2022-12-04 19:31:56 +0100

    Homework 01
    Calculate phone price

---
+++ 01-Homework/app.js
@@ -0,0 +1,9 @@
+let phonePrice = 119.95 ;
+let fivePercentageOfPhonePrice = (phonePrice * 5) / 100 ;
+let resultPhonePrice = phonePrice + fivePercentageOfPhonePrice ;
+
+let sumOfAllPhones = resultPhonePrice *30;
+console.log(sumOfAllPhones);
+
+
+
