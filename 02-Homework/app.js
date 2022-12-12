4f6a067dfc3af1155aa39b88ad06887a18dc2c64
PavlinkaA <pavlinkaatanasova@yahoo.com>
2022-12-12 23:41:21 +0100

    Homework 02
    Chinese year

---
+++ 02-Homework/app.js
@@ -0,0 +1,44 @@
+let year = prompt ("enter a year");
+let result = (year - 4 ) % 12; 
+
+if (result === 0) { 
+    console.log("Rat");
+}
+else if (result === 1) {
+    console.log("Ox")
+}
+else if (result === 2) { 
+    console.log("Tiger")
+}
+else if (result === 3) {
+    console.log ("Rabbit")
+}
+else if( result === 4) { 
+    console.log ("Dragon")
+}
+else if (result === 5) { 
+    console.log("Snake")
+    } 
+    else if (result=== 6 ) {
+        console.log("Horse")
+    }
+    else if (result === 7) { 
+        console.log("Goat")
+    }
+    else if (result === 8) {
+        console.log("Monkey")
+    }
+    else if (result === 9) {
+        console.log ("Rooster")
+    }
+    else if (result === 10) {
+        console.log("Dog")
+    }
+    else if (result === 11) {
+        console.log("Pig")
+    }
+    else {
+        console.log("No info")
+    }
+
+    
